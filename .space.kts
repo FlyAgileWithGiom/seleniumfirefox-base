375a69c2756784f06436d3dad5c4dee3690550b7
Guillaume Duquesnay <guillaume.duquesnay@gmail.com>
2022-10-25 17:35:37 +0000

    init: build docker and run the test

---
+++ .space.kts
@@ -0,0 +1,28 @@
+/**
+* JetBrains Space Automation
+* This Kotlin-script file lets you automate build activities
+* For more info, see https://www.jetbrains.com/help/space/automation.html
+*/
+
+job("create image!") {
+    job("Build image") {duquesnay/wdiobase:build-test
+        CONTAINER_NAME="duquesnay/wdiobase:build-test"
+        docker {
+            build {
+                context = "docker"
+                file = "./Dockerfile"
+                name = "duquesnay/wdiobase:build-test"
+                labels["vendor"] = "fly-agile"
+                args["HTTP_PROXY"] = "http://10.20.30.1:123"
+            }
+        }
+        container(displayName = "wdio fresh build", image = "duquesnay/wdiobase:build-test") {    
+            shellScript {
+                content = """
+                    echo Hello
+                    echo World!
+                """
+            }
+        }
+    }
+}
